14fc9935d22ef21dd05431897b7f126da679074c
Diogo Xavier <diogodsxavier@gmail.com>
2024-08-21 13:55:54 -0300

    configurando sequelize

--- Módulo 13/api-rest/src/routes/homeRoutes.js
+++ Módulo 13/api-rest/src/routes/homeRoutes.js
@@ -1,8 +1,8 @@
 import { Router } from 'express';
+import homeController from '../controllers/HomeController';
+
 const router = new Router();
 
-router.get('/', (req, res) => {
-  res.send('Olá Mundo!');
-})
+router.get('/', homeController.index)
 
 export default router;
