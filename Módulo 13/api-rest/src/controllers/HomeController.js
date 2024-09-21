5283d3836a4afc47d33ed0a818b16a0ebaf1efc5
Diogo Xavier <diogodsxavier@gmail.com>
2024-09-21 17:18:06 -0300

    CRUD nos usuários

--- Módulo 13/api-rest/src/controllers/HomeController.js
+++ Módulo 13/api-rest/src/controllers/HomeController.js
@@ -1,16 +1,6 @@
-import Aluno from '../models/Aluno';
-
 class HomeController {
   async index(req, res) {
-    const novoAluno = await Aluno.create({
-      nome: 'Denize',
-      sobrenome: 'Xavier',
-      email: 'denize@gmail.com',
-      idade: 37,
-      peso: 80,
-      altura: 1.65,
-    });
-    res.json(novoAluno);
+    res.json('Index');
   }
 }
 
