ec310d71a57cf46923b142e32632790278eb86f1
Diogo Xavier <diogodsxavier@gmail.com>
2024-09-25 15:01:32 -0300

    Tratando arquivos enviados

--- Módulo 13/api-rest/src/config/multerConfig.js
+++ Módulo 13/api-rest/src/config/multerConfig.js
@@ -4,7 +4,14 @@ import { extname, resolve } from 'path';
 const aleatorio = () => Math.floor(Math.random() * 10000 + 10000);
 
 export default {
-  Storage: multer.diskStorage({
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype !== 'image/png' && file.mimetype !== 'image/jpeg') {
+      return cb(new multer.MulterError('Arquivo precisa ser JPG ou PNG.'));
+    }
+
+    return cb(null, true);
+  },
+  storage: multer.diskStorage({
     destination: (req, file, cb) => {
       cb(null, resolve(__dirname, '..', '..', 'uploads'));
     },
