040f62764b3186914dc81a71a62cc3d47d53cd27
Diogo Xavier <diogodsxavier@gmail.com>
2025-01-31 16:25:27 -0300

    Estudando

--- Hooks/useContext/src/main.jsx
+++ Hooks/useContext/src/main.jsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './router/Home.jsx'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
