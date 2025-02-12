57fb39d2796183139066e03af91bc2e80ac14ec9
Diogo Xavier <diogodsxavier@gmail.com>
2025-02-12 15:03:10 -0300

    useCallback e useMemo

--- Estudando hooks/useMemo/src/App.jsx
+++ Estudando hooks/useMemo/src/App.jsx
@@ -1,9 +1,29 @@
-import './App.css';
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-empty */
+import { useMemo, useState } from "react";
 
 function App() {
+  const [number, setNumber] = useState(1);
+  const [text, setText] = useState('');
+
+  const doubleNumber = useMemo(() => {
+    return slowFunction(number);
+  }, [number]);
+
   return (
-    <p>oi</p>
+    <div>
+      <p>{number}</p>
+      <input value={text} onChange={e => setText(e.target.value)} />
+      <button onClick={() => setNumber(2)}>Increment</button>
+      <p>text: {text}</p>
+    </div>
   )
 }
+
+function slowFunction(num) {
+  console.log('Slow function is being called!');
+  for (let i = 0; i < 10000; i++) {}
+  return num * 2;  
+};
 
 export default App
