17fbc7d17c625c14f23c0efb954d035cda5b1765
Diogo Xavier <diogodsxavier@gmail.com>
2025-02-14 15:42:35 -0300

    Estudando typescript

--- Estudos de Typescript/comecando-no-typescript/src/index.ts
+++ Estudos de Typescript/comecando-no-typescript/src/index.ts
@@ -1,6 +1,20 @@
+// Tipos no TypescriptT
 let age: string = "Hello World!";
-
-console.log(age);
-
 const names: string[] = ["diogo", "daniel", "silva"];
 const num: number[] = [1, 2, 3, 4, 5, 6];
+let qualqueCoisa: any = 5;
+qualqueCoisa = '1'; 
+
+// Tuples
+
+const peaple: [string, string, number] = ['Diogo', 'Daniel', 20];
+
+// Lista de Tuples
+
+const peaples: [string, number][] = [
+    ['Diogo', 20],
+    ['José', 44],
+    ['Denize', 39],
+    ['Dherik', 16]
+];
+ 
