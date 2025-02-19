e231cbd3c6482ed09a976873c044694f55141893
Diogo Xavier <diogodsxavier@gmail.com>
2025-02-19 15:03:17 -0300

    Construção do App

--- Estudos de React/App-de-previsão-do-tempo/src/components/WeatherApp.jsx
+++ Estudos de React/App-de-previsão-do-tempo/src/components/WeatherApp.jsx
@@ -1,16 +1,35 @@
 /* eslint-disable no-undef */
-import { useState } from "react"
+import { useCallback, useState } from "react"
 
 function WeatherApp() {
      // Estados para termo de busca, dados do clima atual, previsão, loading e erro
-
-     const [searchTerm, setSearchTerm] = useState('São Paulo');
+     const [searchTerm, setSearchTerm] = useState('Guarulhos');
      const [currentWeather, setCurrentWeather] = useState(null);
      const [forecastData, setForecastData] = useState(null);
      const [loading, setLoading] = useState(false);
      const [error, setError] = useState(null);
 
-     const API_KEY = process.env.REACT_APP_OPENWEATHER_API_KEY;
+     // const API_KEY = process.env.REACT_APP_OPENWEATHER_API_KEY;
+
+     // Função para buscar dados do clima e da previsão
+     const fetchWeather = useCallback(async() => {
+          if (!searchTerm) return;
+
+          setLoading(true);
+          setError(null);
+
+          try {
+               // Clima atual
+               const currentResponse = await fetch(
+                    `https://api.openweathermap.org/data/2.5/weather?q=${searchTerm}&appid={API key}`
+               );
+          } catch (error) {
+               setError(error.message);
+          } finally {
+               setLoading(false);
+          }
+     });
+
 };
 
 export default WeatherApp
